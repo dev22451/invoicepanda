ecfd84f80ffc3c0cb9e263a6097f47bb854a3fc9
rohitsnegi1450 <rohitsinghnegi977@gmail.com>
2021-08-04 10:51:41 +0530

    link add

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
-import  React, { useState } from 'react';
+import React, { useState } from 'react';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
-import { Link } from 'react-router-dom';
+import { Link } from '@reach/router';
 import { SidebarData } from './SidebarData';
 import './Navbar.css';
 import { IconContext } from 'react-icons';
