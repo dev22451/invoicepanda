bc5ca7195dd8fccb9df0d1dbfbff4e6eff8c784d
rohitsnegi1450 <rohitsinghnegi977@gmail.com>
2021-08-04 12:08:28 +0530

    add

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,22 +1,33 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+// /**
+//  * Implement Gatsby's Node APIs in this file.
+//  *
+//  * See: https://www.gatsbyjs.com/docs/node-apis/
+//  */
 
-// You can delete this file if you're not using it
+// // You can delete this file if you're not using it
 
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-    if (stage === "build-html" || stage === "develop-html") {
-        actions.setWebpackConfig({
-            module: {
-                rules: [
-                    {
-                        test: /firebase/,
-                        use: loaders.null(),
-                    },
-                ],
-            },
-        })
+// exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+//     if (stage === "build-html" || stage === "develop-html") {
+//         actions.setWebpackConfig({
+//             module: {
+//                 rules: [
+//                     {
+//                         test: /firebase/,
+//                         use: loaders.null(),
+//                     },
+//                 ],
+//             },
+//         })
+//     }
+// }
+
+import * as React from "react"
+// Check if window is defined (so if in the browser or in node.js).
+const isBrowser = typeof window !== "undefined"
+export default function MyComponent() {
+    let loggedIn = false
+    if (isBrowser) {
+        window.localstorage.getItem("isLoggedIn") === "true"
     }
+    return <div>Am I logged in? {loggedIn}</div>
 }
